a77a10b5535fa7975f3a8aba4a61704477add8a3
João Souza Bispo <jsbispo1101@gmail.com>
2026-05-08 17:40:28 -0400

    show front url

--- index.js
+++ index.js
@@ -12,17 +12,17 @@ import { auth } from "./src/config/auth.js";
 const app = express();
 const port = 3000;
 
+env.config();
+db.connect();
+
+console.log(process.env.FRONTEND_URL);
+
 app.use(
 	cors({
 		origin: process.env.FRONTEND_URL,
 		credentials: true,
 	}),
 );
-
-console.log(process.env.FRONTEND_URL);
-
-env.config();
-db.connect();
 
 app.use(cookieParser());
 app.use(express.json());
